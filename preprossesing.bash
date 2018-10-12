2c5ad69cacba1c23927f4ee24d985dfbebe3d61a
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-10-12 21:35:16 +0200

    Update preprossesing.bash

--- preprossesing.bash
+++ preprossesing.bash
@@ -1,72 +1,72 @@
-trim_galore --illumina -q 20 --fastqc -o /root/taka/ATAC-seq/fastq/fastq_trim \
+/root/myPrograms/TrimGalore-0.4.5/trim_galore --illumina -q 20 --fastqc -o /root/taka/ATAC-seq/fastq/fastq_trim \
 10_Y-MPP4_2.fastq.gz &
-trim_galore --illumina -q 20 --fastqc -o /root/taka/ATAC-seq/fastq/fastq_trim \
+/root/myPrograms/TrimGalore-0.4.5/trim_galore --illumina -q 20 --fastqc -o /root/taka/ATAC-seq/fastq/fastq_trim \
 13_A-HSC_1.fastq.gz &
-trim_galore --illumina -q 20 --fastqc -o /root/taka/ATAC-seq/fastq/fastq_trim \
+/root/myPrograms/TrimGalore-0.4.5/trim_galore --illumina -q 20 --fastqc -o /root/taka/ATAC-seq/fastq/fastq_trim \
 14_A-MPP1_1.fastq.gz &
-trim_galore --illumina -q 20 --fastqc -o /root/taka/ATAC-seq/fastq/fastq_trim \
+/root/myPrograms/TrimGalore-0.4.5/trim_galore --illumina -q 20 --fastqc -o /root/taka/ATAC-seq/fastq/fastq_trim \
 15_A-MPP2_1.fastq.gz &
-trim_galore --illumina -q 20 --fastqc -o /root/taka/ATAC-seq/fastq/fastq_trim \
+/root/myPrograms/TrimGalore-0.4.5/trim_galore --illumina -q 20 --fastqc -o /root/taka/ATAC-seq/fastq/fastq_trim \
 16_A-MPP3_1_new.fastq.gz &
-trim_galore --illumina -q 20 --fastqc -o /root/taka/ATAC-seq/fastq/fastq_trim \
+/root/myPrograms/TrimGalore-0.4.5/trim_galore --illumina -q 20 --fastqc -o /root/taka/ATAC-seq/fastq/fastq_trim \
 17_A-MPP4_1_new.fastq.gz &
-trim_galore --illumina -q 20 --fastqc -o /root/taka/ATAC-seq/fastq/fastq_trim \
+/root/myPrograms/TrimGalore-0.4.5/trim_galore --illumina -q 20 --fastqc -o /root/taka/ATAC-seq/fastq/fastq_trim \
 18_A-HSC_2_new.fastq.gz &
-trim_galore --illumina -q 20 --fastqc -o /root/taka/ATAC-seq/fastq/fastq_trim \
+/root/myPrograms/TrimGalore-0.4.5/trim_galore --illumina -q 20 --fastqc -o /root/taka/ATAC-seq/fastq/fastq_trim \
 19_A-MPP1_2_new.fastq.gz &
-trim_galore --illumina -q 20 --fastqc -o /root/taka/ATAC-seq/fastq/fastq_trim \
+/root/myPrograms/TrimGalore-0.4.5/trim_galore --illumina -q 20 --fastqc -o /root/taka/ATAC-seq/fastq/fastq_trim \
 1_Y-HSC_1.fastq.gz &
-trim_galore --illumina -q 20 --fastqc -o /root/taka/ATAC-seq/fastq/fastq_trim \
+/root/myPrograms/TrimGalore-0.4.5/trim_galore --illumina -q 20 --fastqc -o /root/taka/ATAC-seq/fastq/fastq_trim \
 20_A-MPP2_2_new.fastq.gz &
-trim_galore --illumina -q 20 --fastqc -o /root/taka/ATAC-seq/fastq/fastq_trim \
+/root/myPrograms/TrimGalore-0.4.5/trim_galore --illumina -q 20 --fastqc -o /root/taka/ATAC-seq/fastq/fastq_trim \
 21_A-MPP3_2_new.fastq.gz &
-trim_galore --illumina -q 20 --fastqc -o /root/taka/ATAC-seq/fastq/fastq_trim \
+/root/myPrograms/TrimGalore-0.4.5/trim_galore --illumina -q 20 --fastqc -o /root/taka/ATAC-seq/fastq/fastq_trim \
 22_A-MPP4_2_new.fastq.gz &
-trim_galore --illumina -q 20 --fastqc -o /root/taka/ATAC-seq/fastq/fastq_trim \
+/root/myPrograms/TrimGalore-0.4.5/trim_galore --illumina -q 20 --fastqc -o /root/taka/ATAC-seq/fastq/fastq_trim \
 25_Y-GMP_1.fastq.gz &
-trim_galore --illumina -q 20 --fastqc -o /root/taka/ATAC-seq/fastq/fastq_trim \
+/root/myPrograms/TrimGalore-0.4.5/trim_galore --illumina -q 20 --fastqc -o /root/taka/ATAC-seq/fastq/fastq_trim \
 26_Y-MEP_1.fastq.gz &
-trim_galore --illumina -q 20 --fastqc -o /root/taka/ATAC-seq/fastq/fastq_trim \
+/root/myPrograms/TrimGalore-0.4.5/trim_galore --illumina -q 20 --fastqc -o /root/taka/ATAC-seq/fastq/fastq_trim \
 27_Y-CLP_1.fastq.gz &
-trim_galore --illumina -q 20 --fastqc -o /root/taka/ATAC-seq/fastq/fastq_trim \
+/root/myPrograms/TrimGalore-0.4.5/trim_galore --illumina -q 20 --fastqc -o /root/taka/ATAC-seq/fastq/fastq_trim \
 28_A-GMP_1.fastq.gz &
-trim_galore --illumina -q 20 --fastqc -o /root/taka/ATAC-seq/fastq/fastq_trim \
+/root/myPrograms/TrimGalore-0.4.5/trim_galore --illumina -q 20 --fastqc -o /root/taka/ATAC-seq/fastq/fastq_trim \
 29_A-MEP_1.fastq.gz &
-trim_galore --illumina -q 20 --fastqc -o /root/taka/ATAC-seq/fastq/fastq_trim \
+/root/myPrograms/TrimGalore-0.4.5/trim_galore --illumina -q 20 --fastqc -o /root/taka/ATAC-seq/fastq/fastq_trim \
 2_Y-MPP1_1.fastq.gz &
-trim_galore --illumina -q 20 --fastqc -o /root/taka/ATAC-seq/fastq/fastq_trim \
+/root/myPrograms/TrimGalore-0.4.5/trim_galore --illumina -q 20 --fastqc -o /root/taka/ATAC-seq/fastq/fastq_trim \
 30_A-CLP_1.fastq.gz &
-trim_galore --illumina -q 20 --fastqc -o /root/taka/ATAC-seq/fastq/fastq_trim \
+/root/myPrograms/TrimGalore-0.4.5/trim_galore --illumina -q 20 --fastqc -o /root/taka/ATAC-seq/fastq/fastq_trim \
 31_Y-GMP_2.fastq.gz &
-trim_galore --illumina -q 20 --fastqc -o /root/taka/ATAC-seq/fastq/fastq_trim \
+/root/myPrograms/TrimGalore-0.4.5/trim_galore --illumina -q 20 --fastqc -o /root/taka/ATAC-seq/fastq/fastq_trim \
 32_Y-MEP_2_new.fastq.gz &
-trim_galore --illumina -q 20 --fastqc -o /root/taka/ATAC-seq/fastq/fastq_trim \
+/root/myPrograms/TrimGalore-0.4.5/trim_galore --illumina -q 20 --fastqc -o /root/taka/ATAC-seq/fastq/fastq_trim \
 33_Y-CLP_2_new.fastq.gz &
-trim_galore --illumina -q 20 --fastqc -o /root/taka/ATAC-seq/fastq/fastq_trim \
+/root/myPrograms/TrimGalore-0.4.5/trim_galore --illumina -q 20 --fastqc -o /root/taka/ATAC-seq/fastq/fastq_trim \
 34_A-GMP_2.fastq.gz &
-trim_galore --illumina -q 20 --fastqc -o /root/taka/ATAC-seq/fastq/fastq_trim \
+/root/myPrograms/TrimGalore-0.4.5/trim_galore --illumina -q 20 --fastqc -o /root/taka/ATAC-seq/fastq/fastq_trim \
 35_A-MEP_2_new.fastq.gz &
-trim_galore --illumina -q 20 --fastqc -o /root/taka/ATAC-seq/fastq/fastq_trim \
+/root/myPrograms/TrimGalore-0.4.5/trim_galore --illumina -q 20 --fastqc -o /root/taka/ATAC-seq/fastq/fastq_trim \
 36_A-CLP_2_new.fastq.gz &
-trim_galore --illumina -q 20 --fastqc -o /root/taka/ATAC-seq/fastq/fastq_trim \
+/root/myPrograms/TrimGalore-0.4.5/trim_galore --illumina -q 20 --fastqc -o /root/taka/ATAC-seq/fastq/fastq_trim \
 3_Y-MPP2_1.fastq.gz &
-trim_galore --illumina -q 20 --fastqc -o /root/taka/ATAC-seq/fastq/fastq_trim \
+/root/myPrograms/TrimGalore-0.4.5/trim_galore --illumina -q 20 --fastqc -o /root/taka/ATAC-seq/fastq/fastq_trim \
 4_Y-MPP3_1.fastq.gz &
-trim_galore --illumina -q 20 --fastqc -o /root/taka/ATAC-seq/fastq/fastq_trim \
+/root/myPrograms/TrimGalore-0.4.5/trim_galore --illumina -q 20 --fastqc -o /root/taka/ATAC-seq/fastq/fastq_trim \
 5_Y-MPP4_1.fastq.gz &
-trim_galore --illumina -q 20 --fastqc -o /root/taka/ATAC-seq/fastq/fastq_trim \
+/root/myPrograms/TrimGalore-0.4.5/trim_galore --illumina -q 20 --fastqc -o /root/taka/ATAC-seq/fastq/fastq_trim \
 6_Y-HSC_2.fastq.gz &
-trim_galore --illumina -q 20 --fastqc -o /root/taka/ATAC-seq/fastq/fastq_trim \
+/root/myPrograms/TrimGalore-0.4.5/trim_galore --illumina -q 20 --fastqc -o /root/taka/ATAC-seq/fastq/fastq_trim \
 7_Y-MPP1_2.fastq.gz &
-trim_galore --illumina -q 20 --fastqc -o /root/taka/ATAC-seq/fastq/fastq_trim \
+/root/myPrograms/TrimGalore-0.4.5/trim_galore --illumina -q 20 --fastqc -o /root/taka/ATAC-seq/fastq/fastq_trim \
 8_Y-MPP2_2.fastq.gz &
-trim_galore --illumina -q 20 --fastqc -o /root/taka/ATAC-seq/fastq/fastq_trim \
+/root/myPrograms/TrimGalore-0.4.5/trim_galore --illumina -q 20 --fastqc -o /root/taka/ATAC-seq/fastq/fastq_trim \
 9_Y-MPP3_2.fastq.gz &
-trim_galore --illumina -q 20 --fastqc -o /root/taka/ATAC-seq/fastq/fastq_trim \
+/root/myPrograms/TrimGalore-0.4.5/trim_galore --illumina -q 20 --fastqc -o /root/taka/ATAC-seq/fastq/fastq_trim \
 O-HSC_1.fastq.gz &
-trim_galore --illumina -q 20 --fastqc -o /root/taka/ATAC-seq/fastq/fastq_trim \
+/root/myPrograms/TrimGalore-0.4.5/trim_galore --illumina -q 20 --fastqc -o /root/taka/ATAC-seq/fastq/fastq_trim \
 O-HSC_2.fastq.gz &
-trim_galore --illumina -q 20 --fastqc -o /root/taka/ATAC-seq/fastq/fastq_trim \
+/root/myPrograms/TrimGalore-0.4.5/trim_galore --illumina -q 20 --fastqc -o /root/taka/ATAC-seq/fastq/fastq_trim \
 Y-HSC_1.fastq.gz &
-trim_galore --illumina -q 20 --fastqc -o /root/taka/ATAC-seq/fastq/fastq_trim \
+/root/myPrograms/TrimGalore-0.4.5/trim_galore --illumina -q 20 --fastqc -o /root/taka/ATAC-seq/fastq/fastq_trim \
 Y-HSC_2.fastq.gz &
