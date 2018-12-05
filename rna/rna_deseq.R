aec8f53c5eb6fdc0f357c3dd3ad37b325e2eb110
Roberto Tirado Magallanes <rtmag4@gmail.com>
2018-12-05 17:10:13 +0800

    Update rna_deseq.R

--- rna/rna_deseq.R
+++ rna/rna_deseq.R
@@ -42,3 +42,64 @@ colnames(dat)=c("OH1","OH2","OM1_1","OM1_2","OM2_1","OM2_2","OM3_1","OM3_2","OM4
 
 saveRDS(dat,"HSC_counts.rds")
 
+#################################################################################################################################
+countData=readRDS("HSC_counts.rds")
+options(scipen=999)
+library(DESeq2)
+library(gplots)
+library(factoextra)
+library(RColorBrewer)
+
+design<-data.frame(cell=c(
+"HSC","HSC","MPP1","MPP1","MPP2","MPP2","MPP3","MPP3","MPP4","MPP4",
+"HSC","HSC","MPP1","MPP1","MPP2","MPP2","MPP3","MPP3","MPP4","MPP4"
+
+                          ),
+age=c(
+"OLD","OLD","OLD","OLD","OLD","OLD","OLD","OLD","OLD","OLD",
+"YOUNG","YOUNG","YOUNG","YOUNG","YOUNG","YOUNG","YOUNG","YOUNG","YOUNG","YOUNG"
+)
+)
+
+dLRT <- DESeqDataSetFromMatrix(countData = countData, colData = design, design = ~ cell + age )
+dLRT <- DESeq(dLRT, test="LRT", reduced=~1)
+dLRT_vsd <- varianceStabilizingTransformation(dLRT)
+dLRT_res = results(dLRT)
+vsd = assay(dLRT_vsd)
+saveRDS(vsd,"HSC_vsd.rds")
+
+pdf("Diagnostic_pca_all_samples.pdf")
+plotPCA(dLRT_vsd,ntop=50000,intgroup=c("cell","age"))
+dev.off()
+
+
+sig_vsd = vsd[which(dLRT_res$padj<0.05),]
+
+track=as.character(design$cell)
+track[track=="HSC"]=1
+track[track=="MPP1"]=2
+track[track=="MPP2"]=3
+track[track=="MPP3"]=4
+track[track=="MPP4"]=5
+track=as.numeric(track)
+colores=c("#ffdfba","#ffb3ba","#ffffba","#baffc9","#bae1ff")
+clab=as.character(colores[track])
+
+track2=as.character(design$age)
+track2[track2=="OLD"]=1
+track2[track2=="YOUNG"]=2
+track2=as.numeric(track2)
+colores2=c("black","grey")
+clab2=as.character(colores2[track2])
+
+colors <- rev(colorRampPalette( (brewer.pal(9, "RdBu")) )(9))
+
+source("https://raw.githubusercontent.com/rtmag/tumor-meth-pipe/master/heatmap3.R")
+
+png("anova_heatmap.png",width= 3.25,
+  height= 3.25,units="in",
+  res=1200,pointsize=4)
+heatmap.3(sig_vsd,col=colors,scale="row", trace="none",distfun = function(x) get_dist(x,method="pearson"),srtCol=90,
+labRow = FALSE,xlab="", ylab=paste(dim(sig_vsd)[1],"Genes"),key.title="Gene expression",cexCol=.8,ColSideColors=rbind(clab,clab2))
+legend("topright",legend=c("Normal","Melanoma","Nevi","MIS"),fill=c("#ffdfba","#ffb3ba","#baffc9","#bae1ff"), border=T, bty="n" )
+dev.off()
